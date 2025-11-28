91feded5e4d96914bee91693f1750778063a34b9
Andrey <112133823+Notsumez@users.noreply.github.com>
2025-11-28 14:57:46 -0300

    feat: implementa a entidade Developer com propriedades e geração de ID

--- src/developers/entities/developer.entity.ts
+++ src/developers/entities/developer.entity.ts
@@ -1,1 +1,22 @@
-export class Developer {}
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-require-imports
+const { nanoid } = require('nanoid');
+
+@Entity('developers')
+export class Developer {
+  @PrimaryGeneratedColumn('uuid')
+  id: string; // dev_083sdewd987
+  @Column()
+  name: string;
+  @Column()
+  email: string;
+  @Column()
+  dateOfBirth: string;
+
+  @BeforeInsert()
+  generateId() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    this.id = `dev_${nanoid()}`;
+  }
+}
